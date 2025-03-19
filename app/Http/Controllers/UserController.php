aac3448bf85427cadc391032176d9cb1a9b4b1e8
Aleksandr Samokhin <samokhinteam@gmail.com>
2025-03-19 17:10:47 +0800

    Complete all tests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::with('projects')->has('projects')->get();
 
         return view('users.index', compact('users'));
     }
